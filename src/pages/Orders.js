fdc3f6b01f89193e52cf059dccc44568b5319370
Yash Shah <yashshh123@gmail.com>
2021-10-17 12:56:20 +0530

    Common Drawer and code clean up for the page
    Common Drawer and code clean up for the page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -5,134 +5,14 @@ import Helmet from "react-helmet";
 import { Divider as MuiDivider, Grid, Typography } from "@material-ui/core";
 import { spacing } from "@material-ui/system";
 import LoadingScreen from "./LoadingScreen";
-import ConnectivityError from "./ConnectivityError";
 import { useTranslation } from "react-i18next";
-import MUIDataTable from "mui-datatables";
 import { useDispatch, useSelector } from "react-redux";
 import { setToken } from "../redux/actions/userActions";
 import { orders } from "../mockdata/order";
-import moment from "moment";
-import SideDrawer from "./Drawer";
+import OrderTable from "../components/tables/OrderTable";
+import OrdersDrawer from "../components/Drawers/OrdersDrawer";
 const Divider = styled(MuiDivider)(spacing);
-const ViewButton = styled.div`
-  border: 1px solid #000;
-  border-radius: 30px;
-  padding: 3px 10px;
-  text-align: center;
-  cursor: pointer;
-`;
-const SucessfulPayment = styled.div`
-  color: green;
-  font-weight: 800;
-`;
-const PendingPayment = styled.div`
-  color: orange;
-  font-weight: 800;
-`;
-const FailedPayment = styled.div`
-  color: red;
-  font-weight: 800;
-`;
-const columns = [
-  {
-    name: "created_at",
-    label: "Date",
-    options: {
-      filter: false,
-      sort: true,
-      customBodyRender: (value) => (
-        <>{moment(value).format("DD-MM-YYYY HH:MM:SS")} </>
-      ),
-    },
-  },
-  {
-    name: "FullName",
-    label: "Full Name",
-    options: {
-      filter: false,
-      sort: false,
-    },
-  },
-  {
-    name: "Email",
-    label: "Email",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "ProductType",
-    label: "Product Type",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  // {
-  //   name: "Type",
-  //   label: "Order Type",
-  //   options: {
-  //     filter: true,
-  //     sort: false,
-  //   }
-  // },
-  {
-    name: "amount",
-    label: "Amount",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "Status",
-    label: "Status",
-    options: {
-      filter: true,
-      sort: false,
-      customBodyRender: (value) => {
-        console.log(value);
-        return (
-          <>
-            {value === "paid" && <SucessfulPayment>Paid </SucessfulPayment>}
-            {value === "pending" && <PendingPayment>Pending </PendingPayment>}
-            {value === "failed" && <FailedPayment>Failed </FailedPayment>}
-          </>
-        );
-      },
-    },
-  },
-  {
-    name: "actions",
-    label: "Actions",
-    options: {
-      filter: true,
-      sort: false,
-      customBodyRender: () => <ViewButton>View</ViewButton>,
-    },
-  },
-];
 
-const options = {
-  filterType: "checkbox",
-  download: false,
-  print: false,
-};
-function EnhancedTable() {
-  const updatedOrder = orders.map((order) => {
-    order.FullName = `${order.FirstName} ${order.LastName}`;
-    order.amount = `${order.Amount} ${order.Currency} `;
-    return order;
-  });
-  const [data, setData] = React.useState(updatedOrder);
-  const [error, setError] = useState(false);
-  useEffect(() => {}, []);
-  if (error) {
-    return <ConnectivityError />;
-  }
-  return <MUIDataTable data={data} columns={columns} options={options} />;
-}
 function Orders() {
   const [pageLoaded, setPageLoaded] = useState(false);
   const keycloak = useSelector((state) => state.userReducer.keycloak);
@@ -165,12 +45,8 @@ function Orders() {
         <Divider my={6} />
         <Grid container spacing={6}>
           <Grid item xs={12}>
-            <EnhancedTable />
-            <SideDrawer
-              open={openDrawer}
-              close={() => setOpenDrawer(false)}
-              data={""}
-            />
+            <OrderTable orders={orders} openDrawer={() => setOpenDrawer(true)} />
+            <OrdersDrawer open={openDrawer} close={() => setOpenDrawer(false)} />
           </Grid>
         </Grid>
       </React.Fragment>
--- src/pages/Payments.js
+++ src/pages/Payments.js
@@ -5,142 +5,14 @@ import Helmet from "react-helmet";
 import { Divider as MuiDivider, Grid, Typography } from "@material-ui/core";
 import { spacing } from "@material-ui/system";
 import LoadingScreen from "./LoadingScreen";
-import ConnectivityError from "./ConnectivityError";
 import { useTranslation } from "react-i18next";
-import MUIDataTable from "mui-datatables";
 import { useDispatch, useSelector } from "react-redux";
 import { setToken } from "../redux/actions/userActions";
 import { payments } from "../mockdata/payments";
-import moment from "moment";
-import SideDrawer from "./Drawer";
+import PaymentsTable from "../components/tables/PaymentsTable";
+import PaymentsDrawer from "../components/Drawers/PaymentsDrawer";
 const Divider = styled(MuiDivider)(spacing);
-const ViewButton = styled.div`
-  border: 1px solid #000;
-  border-radius: 30px;
-  padding: 3px 10px;
-  text-align: center;
-  cursor: pointer;
-`;
-const SucessfulPayment = styled.div`
-  color: green;
-  font-weight: 800;
-`;
-const PendingPayment = styled.div`
-  color: orange;
-  font-weight: 800;
-`;
-const FailedPayment = styled.div`
-  color: red;
-  font-weight: 800;
-`;
-const columns = [
-  {
-    name: "created_at",
-    label: "Date",
-    options: {
-      filter: false,
-      sort: false,
-      customBodyRender: (value) => (
-        <>{moment(value).format("DD-MM-YYYY HH:MM:SS")} </>
-      ),
-      // display: false,
-    },
-  },
-  {
-    name: "ParamX",
-    label: "ParamX",
-    options: {
-      filter: false,
-      sort: false,
-    },
-  },
-  {
-    name: "accountName",
-    label: "Account Name",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "amount",
-    label: "Amount",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "CCNumber",
-    label: "Credit Card",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "CCExpDate",
-    label: "Expiry Date",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "ProductType",
-    label: "Charge Type",
-    options: {
-      filter: true,
-      sort: false,
-    },
-  },
-  {
-    name: "PaymentStatus",
-    label: "Status",
-    options: {
-      filter: true,
-      sort: false,
-      customBodyRender: (value) => {
-        console.log(value);
-        return (
-          <>
-            {value === "success" && (
-              <SucessfulPayment>Success </SucessfulPayment>
-            )}
-            {value === "pending" && <PendingPayment>Pending </PendingPayment>}
-            {value === "failed" && <FailedPayment>Failed </FailedPayment>}
-          </>
-        );
-      },
-    },
-  },
-  {
-    name: "actions",
-    label: "Actions",
-    options: {
-      filter: true,
-      sort: false,
-      customBodyRender: (value) => <ViewButton>View</ViewButton>,
-    },
-  },
-];
 
-const options = {
-  filterType: "checkbox",
-  download: false,
-  print: false,
-};
-
-function EnhancedTable() {
-  const [data, setData] = React.useState(payments);
-  const [error, setError] = useState(false);
-  useEffect(() => {}, []);
-  if (error) {
-    return <ConnectivityError />;
-  }
-  console.log(data);
-  return <MUIDataTable data={data} columns={columns} options={options} />;
-}
 function Payments() {
   const [pageLoaded, setPageLoaded] = useState(false);
   const [openDrawer, setOpenDrawer] = useState(false);
@@ -173,11 +45,10 @@ function Payments() {
         <Divider my={6} />
         <Grid container spacing={6}>
           <Grid item xs={12}>
-            <EnhancedTable />
-            <SideDrawer
+            <PaymentsTable payments={payments} openDrawer={() => setOpenDrawer(true)} />
+            <PaymentsDrawer
               open={openDrawer}
               close={() => setOpenDrawer(false)}
-              data={""}
             />
           </Grid>
         </Grid>
