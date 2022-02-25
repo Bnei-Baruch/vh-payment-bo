6ff0e4d83186f75239de78ea8f0f56832e76fbc6
Yash Shah <yashshh123@gmail.com>
2022-02-26 00:15:22 +0530

    Update config.js

--- config-templates/config.js
+++ config-templates/config.js
@@ -2,8 +2,7 @@ window.APP_CONFIG = {
     BUILD_TIME: '{{ (time.Now).UTC }}',
     REG_BRANCH: '{{env.Getenv "CI_COMMIT_REF_NAME" "-"}}',
     REF_VERSION: '{{env.Getenv "CI_COMMIT_SHA" "-"}}',
-    BASE_STATIC_PATH: "/static",
-    PROFILE_URL: '{{env.Getenv "KEYCLOAK_PROFILE_URL" "https://api.eurokab.info/profile/v1/profile"}}',
+    VH_SRV_REGISTRATION: '{{env.Getenv "VH_SRV_REGISTRATION" "https://api.eurokab.info/profile/v1/profile"}}',
     KEYCLOAK_CONFIG: {
         realm: '{{env.Getenv "KEYCLOAK_REALM" "master"}}',
         url: '{{env.Getenv "KEYCLOAK_URL" "https://auth.2serv.eu/auth/"}}',
