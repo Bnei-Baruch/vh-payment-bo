710ffd8b259a5994835cb1c003315acda82458af
Althaf M <allupaku@gmail.com>
2022-03-23 11:05:30 +0000

    import file name fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,7 +4,8 @@ import themeReducer from "./themeReducers";
 import profileReducer from "./profileReducers";
 import settingsReducer from "./settingsReducers";
 import streamReducer from "./streamReducer";
-import userReducer from "./userReducer1";
+import userReducer from "./userReducer";
+
 
 export default combineReducers({
   themeReducer,
