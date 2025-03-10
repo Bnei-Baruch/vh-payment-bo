eadcd030566647438b8fcc3157aaa408ee14c18e
VadymJetSoftPro <vadym.kurakh@jetsoftpro.com>
2025-03-10 13:15:59 +0200

    Added a notification after the search

--- src/pages/Customers/Search/Search.js
+++ src/pages/Customers/Search/Search.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-import { Button, Grid, TextField, Select, MenuItem } from "@material-ui/core";
+import {
+  Button,
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  Snackbar,
+} from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import MUIDataTable from "mui-datatables";
+import { Alert } from "@material-ui/lab";
 import styled from "styled-components";
 
 import { useData } from "./useData";
@@ -16,12 +24,14 @@ const StyledGrid = styled(Grid)`
 export default function Search() {
   const { t } = useTranslation();
   const {
+    alert,
     options,
     columns,
     loading,
     queryType,
     onKeyDown,
     searchQuery,
+    onHideAlert,
     setQueryType,
     searchResult,
     onPressSearch,
@@ -29,43 +39,60 @@ export default function Search() {
   } = useData();
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <TextField
-          id="outlined-basic"
-          label={t("Search.name")}
-          variant="outlined"
-          value={searchQuery}
-          className="s-text-field"
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyDown={onKeyDown}
-        />
-        <Select
-          value={queryType}
-          className="query-type-selector"
-          onChange={(e) => setQueryType(e.target.value)}
-        >
-          <MenuItem value="email">{t("Search.mail")}</MenuItem>
-          <MenuItem value="name">{t("Search.nameLabel")}</MenuItem>
-          <MenuItem value="paramX">{t("Search.paramX")}</MenuItem>
-        </Select>
-        <Button
-          variant="contained"
-          color="primary"
-          className="s-search-btn"
-          onClick={onPressSearch}
-        >
-          {t("Search.name")}
-        </Button>
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label={t("Search.name")}
+            variant="outlined"
+            value={searchQuery}
+            className="s-text-field"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={onKeyDown}
+          />
+          <Select
+            value={queryType}
+            className="query-type-selector"
+            onChange={(e) => setQueryType(e.target.value)}
+          >
+            <MenuItem value="email">{t("Search.mail")}</MenuItem>
+            <MenuItem value="name">{t("Search.nameLabel")}</MenuItem>
+            <MenuItem value="paramX">{t("Search.paramX")}</MenuItem>
+          </Select>
+          <Button
+            variant="contained"
+            color="primary"
+            className="s-search-btn"
+            onClick={onPressSearch}
+          >
+            {t("Search.name")}
+          </Button>
+        </Grid>
+        <StyledGrid item xs={12}>
+          <MUIDataTable
+            title={"Search Result"}
+            data={loading ? [] : searchResult}
+            columns={columns}
+            options={options}
+          />
+        </StyledGrid>
       </Grid>
-      <StyledGrid item xs={12}>
-        <MUIDataTable
-          title={"Search Result"}
-          data={loading ? [] : searchResult}
-          columns={columns}
-          options={options}
-        />
-      </StyledGrid>
-    </Grid>
+      <Snackbar
+        open={alert.visible}
+        onClose={onHideAlert}
+        autoHideDuration={10000}
+      >
+        <Alert
+          icon={false}
+          severity="info"
+          variant="filled"
+          onClose={onHideAlert}
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {alert.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
